65dc039c18dd994a83b6507aa408843feaac01f5
gustavo Feliciano <gustavo08_fonseca@outlook.com>
2021-03-22 00:34:28 -0300

    Adicionando o auth do google e Facebook, ainda com um erro

--- web/js/firebaseAuthFeatures.js
+++ web/js/firebaseAuthFeatures.js
@@ -1,3 +1,66 @@
+//import firebase from "firebase/app";
+//import "firebase/auth";
+
+function entrarFacebook(){
+  var provider = new firebase.auth.FacebookAuthProvider();
+
+  firebase
+  .auth()
+  .signInWithRedirect(provider)
+  .then((result) => {
+    /** @type {firebase.auth.OAuthCredential} */
+    var credential = result.credential;
+
+    // The signed-in user info.
+    var user = result.user;
+
+    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+    var accessToken = credential.accessToken;
+
+    // ...
+  })
+  .catch((error) => {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+
+    window.alert(errorCode,errorMessage,email,credential);
+    // ...
+  });
+}
+
+function entrarGoogle(){
+
+  var provider = new firebase.auth.GoogleAuthProvider();
+
+  window.alert(provider);
+  firebase.auth()
+  .signInWithRedirect(provider)
+  .then((result) => {
+    /** @type {firebase.auth.OAuthCredential} */
+    var credential = result.credential;
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+    window.alert(errorCode,errorMessage,email,credential);
+  });
+}
+
 function logar(){
     let entrarEmail = document.getElementById("entrarEmail").value;
     let entrarSenha = document.getElementById("entrarSenha").value;
@@ -30,6 +93,8 @@ function inscreverse(){
     window.alert("erro ao cadastrar conta!");
   });
 }
+
+
 
 function usuarioLogado(){
     try{
