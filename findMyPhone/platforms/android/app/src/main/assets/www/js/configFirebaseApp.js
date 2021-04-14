3eba72ad19aaaf9cd05fd01a947bc7d4eb869920
isabelasousa1708 <isa_ramos17@hotmail.com>
2021-04-13 21:22:37 -0300

    Script para redefinir senha

---
+++ findMyPhone/platforms/android/app/src/main/assets/www/js/configFirebaseApp.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+        apiKey: "AIzaSyDxei5EZl3HamhgU-RHL_PkNT6fswcz87k",
+        authDomain: "findmyphone-be2c3.firebaseapp.com",
+        projectId: "findmyphone-be2c3",
+        storageBucket: "findmyphone-be2c3.appspot.com",
+        messagingSenderId: "756525455405",
+        appId: "1:756525455405:web:4aa6c5f6bff2e077f800a4",
+        measurementId: "G-J9X54ELH75"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
---
+++ findMyPhone/platforms/android/app/src/main/assets/www/js/firebaseAuthFeatures.js
@@ -0,0 +1,106 @@
+//import firebase from "firebase/app";
+//import "firebase/auth";
+
+function entrarFacebook(){
+  var provider = new firebase.auth.FacebookAuthProvider();
+
+  firebase
+  .auth()
+  .signInWithRedirect(provider)
+  .then((result) => {
+    /** @type {firebase.auth.OAuthCredential} */
+    var credential = result.credential;
+
+    // The signed-in user info.
+    var user = result.user;
+
+    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+    var accessToken = credential.accessToken;
+
+    // ...
+  })
+  .catch((error) => {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+
+    window.alert(errorCode,errorMessage,email,credential);
+    // ...
+  });
+}
+
+function entrarGoogle(){
+
+  var provider = new firebase.auth.GoogleAuthProvider();
+
+  window.alert(provider);
+  firebase.auth()
+  .signInWithRedirect(provider)
+  .then((result) => {
+    /** @type {firebase.auth.OAuthCredential} */
+    var credential = result.credential;
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+    window.alert(errorCode,errorMessage,email,credential);
+  });
+}
+
+function logar(){
+    let entrarEmail = document.getElementById("entrarEmail").value;
+    let entrarSenha = document.getElementById("entrarSenha").value;
+
+    firebase.auth().signInWithEmailAndPassword(entrarEmail, entrarSenha)
+        .then((user) => {
+        window.alert("conta logada com sucesso!");
+        window.location.href = "mapa.html";
+    })
+        .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        window.alert("senha incorreta ou usuário inexistente!");
+     });
+}
+
+function inscreverse(){
+    let criarEmail = document.getElementById("criarEmail").value;
+    let criarSenha = document.getElementById("criarSenha").value;
+    
+    firebase.auth().createUserWithEmailAndPassword(criarEmail, criarSenha)
+  .then((user) => {
+    console.log(user);
+    window.alert("conta criada com sucesso!");
+    window.location.href = "https://projetointegrado-etec2021.herokuapp.com/index.html";
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    window.alert("erro ao cadastrar conta!");
+  });
+}
+
+
+
+function usuarioLogado(){
+    try{
+        let u = firebase.auth().currentUser;
+        return u.entrarEmail;
+    }catch(e){
+        return e;
+    }
+}
